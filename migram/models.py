39424de051193509f03c9cde32b6c47ed4e84fc0
Moses Karani <karanim594@gmail.com>
2022-06-03 17:06:59 +0300

    created a post model

--- migram/models.py
+++ migram/models.py
@@ -1,3 +1,11 @@
+from django.utils import timezone
 from django.db import models
 
+
+
 # Create your models here.
+class Posts(models.Model):
+    author = models.ForeignKey('auth.user', on_delete=models.CASCADE)
+    image = models.ImageField(blank=True, null=True)
+    caption = models.TextField()
+    created_time = models.DateTimeField(default=timezone.now)
