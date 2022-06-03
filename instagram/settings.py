2f25f2c654868614a6481aee0792875c2a2eee60
Moses Karani <karanim594@gmail.com>
2022-06-03 17:12:15 +0300

    connected postgresql and made a migration

--- instagram/settings.py
+++ instagram/settings.py
@@ -77,8 +77,10 @@ WSGI_APPLICATION = 'instagram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'posts',
+        'USER': 'moringa',
+        'PASSWORD': 'morces'
     }
 }
 
