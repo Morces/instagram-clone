5453966a9064809a29cf0b068765388ca4a85e17
Moses Karani <karanim594@gmail.com>
2022-06-04 15:04:32 +0300

    added a classAdmin

--- migram/admin.py
+++ migram/admin.py
@@ -4,3 +4,9 @@ from migram.models import Image
 
 # Register your models here.
 admin.site.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    list_display=('__str__', 'image_name', 'image', 'created', 'modified')
+    readonly_fields=('created','modified')
+    list_editable=('image_name', 'image')
+    search_fields=('profile__user__email','profile__user__username','profile__user__first_name', 'profile__user__last_name', 'image_name')
+    list_filter=('created', 'modified', 'profile__user__is_active','profile__user__is_staff')
